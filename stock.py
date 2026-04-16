240b34cac4dd26c7bc88634b3804cfb9101d8e46
ch25572557-png <ch25572557@gmail.com>
2026-04-16 22:31:22 +0700

    Implement stock management functions in stock.py
    Added functions to load, save, and manage stock data in JSON format.

--- stock.py
+++ stock.py
@@ -1,1 +1,28 @@
+import json
 
+FILE = "stock.json"
+
+def load():
+    return json.load(open(FILE,"r"))
+
+def save(data):
+    json.dump(data, open(FILE,"w"), indent=4)
+
+
+def get_price(item):
+    data = load()
+    return data[item]["price"]
+
+
+def reduce_stock(item, qty):
+    data = load()
+
+    if item not in data:
+        return False
+
+    if data[item]["stock"] < qty:
+        return False
+
+    data[item]["stock"] -= qty
+    save(data)
+    return True
