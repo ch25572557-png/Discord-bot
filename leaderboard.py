c9546b1f505480abeaf1423e379d26677fbf8574
ch25572557-png <ch25572557@gmail.com>
2026-04-17 01:33:36 +0700

    Update leaderboard.py

--- leaderboard.py
+++ leaderboard.py
@@ -3,10 +3,11 @@ from points import load
 
 class LeaderboardView(discord.ui.View):
 
-    @discord.ui.button(label="🏆 Top Points", style=discord.ButtonStyle.blurple)
+    @discord.ui.button(label="🏆 Leaderboard", style=discord.ButtonStyle.blurple)
     async def show(self, interaction, button):
 
         data = load()
+
         top = sorted(data.items(), key=lambda x: x[1], reverse=True)[:10]
 
         text = "\n".join([f"{i+1}. {u} - {p}" for i,(u,p) in enumerate(top)])
