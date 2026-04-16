0a2b8250dff276645d8999d8d29f756cf3643b6d
ch25572557-png <ch25572557@gmail.com>
2026-04-17 01:07:34 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,12 @@
+import json
+import os
 
+def load_json(file):
+    if not os.path.exists(file):
+        return {}
+    with open(file, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+def save_json(file, data):
+    with open(file, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
