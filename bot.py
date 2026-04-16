e5ac0ca16708e50dcba63ecd5baa810b97d9fcad
ch25572557-png <ch25572557@gmail.com>
2026-04-16 22:19:40 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,22 @@
+import discord
+from discord import app_commands
+from ticket import TicketView
 
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+@tree.command(name="ticket")
+async def ticket(i: discord.Interaction):
+
+    await i.channel.send("กดสั่งของ", view=TicketView())
+    await i.response.send_message("เปิดแล้ว",ephemeral=True)
+
+@client.event
+async def on_ready():
+    await tree.sync()
+    print("Ready")
+
+client.run("TOKEN")
