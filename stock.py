56d9d46505e8a3f2c4bebf149e97d84df614bfca
ch25572557-png <ch25572557@gmail.com>
2026-04-17 00:06:28 +0700

    Handle JSON load errors and improve stock functions

--- stock.py
+++ stock.py
@@ -3,24 +3,30 @@ import json
 FILE = "stock.json"
 
 def load():
-    return json.load(open(FILE,"r"))
+    try:
+        return json.load(open(FILE,"r"))
+    except:
+        return {}
 
 def save(data):
     json.dump(data, open(FILE,"w"), indent=4)
 
 
 def get_price(item):
+    return load().get(item, {}).get("price", 0)
+
+
+def add_stock(item, qty):
     data = load()
-    return data[item]["price"]
+    data.setdefault(item, {"stock": 0, "price": 0})
+    data[item]["stock"] += qty
+    save(data)
 
 
 def reduce_stock(item, qty):
     data = load()
 
-    if item not in data:
-        return False
-
-    if data[item]["stock"] < qty:
+    if item not in data or data[item]["stock"] < qty:
         return False
 
     data[item]["stock"] -= qty
