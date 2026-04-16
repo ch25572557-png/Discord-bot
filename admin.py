bf7e6bc08b3e4702e2b2ee9a706f1e4947f93256
ch25572557-png <ch25572557@gmail.com>
2026-04-17 01:32:43 +0700

    Update admin.py

--- admin.py
+++ admin.py
@@ -8,35 +8,28 @@ class AdminStockView(discord.ui.View):
     async def add(self, interaction, button):
 
         await interaction.response.send_message("item qty", ephemeral=True)
-
         msg = await interaction.client.wait_for("message")
 
         item, qty = msg.content.split()
-
         add_stock(item, int(qty))
 
         await interaction.channel.send(f"➕ {item} +{qty}")
-
 
     @discord.ui.button(label="➖ ลดสต๊อก", style=discord.ButtonStyle.red)
     async def remove(self, interaction, button):
 
         await interaction.response.send_message("item qty", ephemeral=True)
-
         msg = await interaction.client.wait_for("message")
 
         item, qty = msg.content.split()
-
         reduce_stock(item, int(qty))
 
         await interaction.channel.send(f"➖ {item} -{qty}")
-
 
     @discord.ui.button(label="📊 ดูสต๊อก", style=discord.ButtonStyle.blurple)
     async def show(self, interaction, button):
 
         data = stock_load()
-
         text = "\n".join([f"{k}: {v['stock']}" for k,v in data.items()])
 
         await interaction.response.send_message(text or "empty", ephemeral=True)
@@ -48,11 +41,9 @@ class AdminPointsView(discord.ui.View):
     async def add(self, interaction, button):
 
         await interaction.response.send_message("user_id amt", ephemeral=True)
-
         msg = await interaction.client.wait_for("message")
 
         uid, amt = msg.content.split()
-
         add_points(uid, int(amt))
 
         await interaction.channel.send(f"➕ {uid} +{amt}")
