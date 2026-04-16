cb91a50b460b624601450698d000e18b28a894d1
ch25572557-png <ch25572557@gmail.com>
2026-04-16 22:25:21 +0700

    Implement async send_log function for logging
    Add asynchronous logging function to send messages.

--- logs.py
+++ logs.py
@@ -1,1 +1,9 @@
+async def send_log(client, text):
 
+    import json
+    config = json.load(open("config.json"))
+
+    channel = client.get_channel(config["log_channel_id"])
+
+    if channel:
+        await channel.send(text)
