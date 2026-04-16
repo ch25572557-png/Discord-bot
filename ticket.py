eb1eddde41b06175f2a9dd7ee20d22bf9d051db3
ch25572557-png <ch25572557@gmail.com>
2026-04-16 22:23:44 +0700

    Update ticket.py

--- ticket.py
+++ ticket.py
@@ -1,1 +1,10 @@
+import discord
+from shop import ShopView
 
+class TicketView(discord.ui.View):
+
+    @discord.ui.button(label="🛒 สั่งของ", style=discord.ButtonStyle.green)
+    async def buy(self, i, b):
+
+        await i.channel.send("🛒 ร้าน", view=ShopView())
+        await i.response.send_message("เปิดร้านแล้ว",ephemeral=True)
