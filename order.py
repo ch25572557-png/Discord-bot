75126d78499bab9d354c26eea94cfa3e605ecb71
ch25572557-png <ch25572557@gmail.com>
2026-04-16 22:32:53 +0700

    Implement order management functions
    Added functions to load and save orders in JSON format, and create a new order with user details.

--- order.py
+++ order.py
@@ -1,1 +1,31 @@
+import json
 
+FILE = "orders.json"
+
+def load():
+    try:
+        return json.load(open(FILE,"r"))
+    except:
+        return {"counter":0,"data":{}}
+
+def save(data):
+    json.dump(data, open(FILE,"w"), indent=4)
+
+
+def create_order(user_id, item, qty, total):
+
+    db = load()
+
+    db["counter"] += 1
+    oid = str(db["counter"])
+
+    db["data"][oid] = {
+        "user_id": user_id,
+        "item": item,
+        "qty": qty,
+        "total": total,
+        "status": "📥 รับออเดอร์"
+    }
+
+    save(db)
+    return oid
