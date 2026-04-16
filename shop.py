4d601b86ddbbe2fbed0a8d9db022b0da92f14068
ch25572557-png <ch25572557@gmail.com>
2026-04-16 22:24:44 +0700

    Update shop.py

--- shop.py
+++ shop.py
@@ -1,1 +1,36 @@
+import discord
+from stock import get_price, reduce_stock
+from order import create_order
+from points import add_points
+from discount import apply
+from logs import send_log
 
+class ShopView(discord.ui.View):
+
+    @discord.ui.button(label="Cash", style=discord.ButtonStyle.green)
+    async def cash(self, i, b):
+        await self.buy(i, "Cash")
+
+    @discord.ui.button(label="Gun", style=discord.ButtonStyle.red)
+    async def gun(self, i, b):
+        await self.buy(i, "Gun")
+
+    async def buy(self, i, item):
+
+        price = get_price(item)
+
+        if not reduce_stock(item,1):
+            return await i.response.send_message("หมด",ephemeral=True)
+
+        total = price
+
+        oid = create_order(i.user.id,item,1,total)
+
+        add_points(i.user.id,1)
+
+        final,d,cost = apply(i.user.id,total)
+
+        await i.response.send_message(
+            f"📦 #{oid}\n{item}\n💰 {final}",
+            ephemeral=True
+        )
